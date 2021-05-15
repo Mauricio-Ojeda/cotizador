63c9ecb572e0dc0121c4a31c48543ec88c8ad4d6
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2021-05-15 16:47:30 -0300

    Creando Formulario video 88

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+import styled from '@emotion/styled';
+
+const Container = styled.div` 
+    max-width: 600px;
+    margin: 0 auto;
+`;
+const ContainerForm = styled.div`
+    background-color: #ffffff;
+    padding: 3rem;
+`;
 
 function App() {
   return (
-    <div >
-      <Header
-        title="Cotizador de Seguros"
-      />
-    </div>
+    <Container >
+        <Header
+          title="Cotizador de Seguros"
+        />
+        <ContainerForm>
+            <Formulario
+              
+            />
+        </ContainerForm>
+    </Container>
   );
 }
 
